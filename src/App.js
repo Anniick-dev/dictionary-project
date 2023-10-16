37ddf71301882098a060352acf6e08331e07345c
Anniick-dev <141575965+Anniick-dev@users.noreply.github.com>
2023-10-16 15:57:34 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Dictionary from './Dictionary';
 function App() {
   return (
     <div className="App">
-      <h1 className="title"> Dictionary </h1>   
+      <h1 className="title"> Dictionary Test </h1>   
       <Dictionary />
     </div>
   );
