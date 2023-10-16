ac1cd589d40f31af220c36461dbc2e98048fc4e9
Anniick-dev <141575965+Anniick-dev@users.noreply.github.com>
2023-10-16 18:34:36 +0200

    Removed 'test' from header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Dictionary from './Dictionary';
 function App() {
   return (
     <div className="App">
-      <h1 className="title"> Dictionary Test </h1>   
+      <h1 className="title"> Dictionary </h1>   
       <Dictionary />
       <footer className='footer'>Coded by <a href="https://github.com/Anniick-dev/dictionary-project"> Annick </a></footer>
     </div>
